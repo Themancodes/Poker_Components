7572f1b691380603dfe18be204fa9829bb62a24d
themancodes <menocorde04@gmail.com>
2021-06-25 19:00:50 -0500

    Feat: Divided into funtional components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
+
+const Card = (props) => {
+  const isNumber = !isNaN(props.number)
+  const [isFlipped, setIsFlipped] = useState(props.flipped)
+
+  return <div 
+    symbol={props.symbol}
+    number={props.number}
+    className={['card', (isFlipped ? 'flipped' : '')].filter(Boolean).join(' ')}
+    onClick={() => { setIsFlipped(!isFlipped) }} 
+    >
+   <div className="container">
+     
+     <div className="front">
+       
+       <div className="card-corner">
+         <div>{ props.number }</div>
+         <div>{ props.symbol }</div>
+       </div>
+
+       <div className="symbols">
+         {(props.number === 'A') && <div>{props.symbol}</div>}
+         {(['J','Q','K'].includes(props.number)) && <div className="image"></div>}
+         {(isNumber) && new Array(parseInt(props.number))
+            .fill(props.symbol)
+            .map((cardSymbol, index) => <div key={index} >{cardSymbol}</div> )
+         }
+       </div>
+       
+       <div className="card-corner">
+       <div>{ props.number }</div>
+        <div>{ props.symbol }</div>
+       </div>
+     
+     </div>
+         <div className="back"></div>
+   </div>
+  
+  </div>
+}
+
+
+const Deck = (props) => {
+  const [cards, setCards] = useState([])
+
+  useEffect((async () => {
+      let cards = await (await fetch(`http://192.168.1.64:8080/${props.path}`)).json()
+        setCards(cards)
+    }))
+
+  return <div>
+      { (cards.length === 0) ?
+      <div>Loading...</div> :
+      <div>
+        <h3>{props.title}</h3>
+        <div className="deck">{ cards.map((card, index) => {
+            const number = card.slice(0, -1)
+            const symbol = card.slice(-1)
+
+            return <Card symbol={symbol} number={number} key={index} flipped={parseInt(props.flipped) > index} />
+          }) }</div>
+      </div>
+      } 
+    </div>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App"> 
+      <header className="App-header"> 
+        <Deck title="Table" path="table/" flipped="3" />
+        <Deck title="Hand" path="deck/2" />
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
